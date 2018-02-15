673ab6e2f198ad22a5b9c611c2c2dcfe1e728545
Tyler Duffus <tyled94@hotmail.com>
2018-02-16 05:42:23 +1000

    Removed: Map assets.

--- space.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="space" tilewidth="32" tileheight="32" tilecount="9" columns="9">
- <image source="F:/Habitat-git/MapEditor/space.png" width="288" height="32"/>
-</tileset>
