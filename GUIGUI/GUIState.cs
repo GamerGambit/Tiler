780c4a99ba4954cc5b165865a22423c2cd516032
Saša Barišić <cartman300@net.hr>
2018-04-03 01:39:15 +0200

    Added GUI project and Utils

---
+++ GUIGUI/GUIState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUIGUI {
+	public delegate void GUIDrawingFunc();
+
+	public class GUIState {
+		public GUIDrawingFunc OnDraw;
+
+		public void Update() {
+
+		}
+
+		public void Draw() {
+
+		}
+	}
+}
---
+++ Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiler {
+	public static class Utils {
+		public static float Clamp(float Value, float Min, float Max) {
+			return Math.Max(Math.Min(Value, Max), Min);
+		}
+	}
+}
