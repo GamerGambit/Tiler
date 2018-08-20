4e0fb3f8fb2c13e26b7ad31dbb32a1dfb3cbc579
Tyler Duffus <tyled94@hotmail.com>
2018-08-21 05:34:12 +1000

    Added: Axis-Aligned Bounding Box class in Physics namespace.

---
+++ Physics/AABB.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace Tiler.Physics
+{
+	public class AABB
+	{
+		public Vector2 Position;
+		public Vector2 Size;
+
+		public bool Contains(AABB other)
+		{
+			if (
+				other.Position.X >= Position.X && other.Position.X <= Position.X + Size.X &&
+				other.Position.Y >= Position.Y && other.Position.Y <= Position.Y + Size.Y
+				)
+				return true;
+
+			return false;
+		}
+	}
+}
